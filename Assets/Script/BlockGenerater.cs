5a8f02a2a2a03239c867cd4f1ad060f42fc37a06
Kousuke-Mitsugi <75293501+Kousuke-Mitsugi@users.noreply.github.com>
2021-01-11 23:51:49 +0900

    Add files via upload

---
+++ Assets/Script/BlockGenerater.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockGenerater : MonoBehaviour
+{
+
+    public GameObject block;
+    float rotspeed = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = -5; i < 5; i++)
+        {
+            for (int j = -5; j < 5; j++)
+            {
+                var newblock = (GameObject)Instantiate(block);
+                newblock.transform.parent = this.transform;
+                newblock.transform.localPosition = new Vector3(i * 3, j * 3 , 0);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 1.0f; //1秒間に3D空間を1移動する
+        Vector3 pos = this.transform.localEulerAngles;
+        pos.z += speed * Time.deltaTime;
+        this.transform.localEulerAngles = pos;
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public GameObject ResetUI;
+	private bool isGameover = false;
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float speed = 5;
+		float x = Input.GetAxisRaw("Horizontal");
+		this.transform.position -= Vector3.left * x * speed * Time.deltaTime;
+
+		var ball = GameObject.Find("Ball");
+		if ((ball.transform.position.y < -25.0) && !isGameover)
+		{
+			isGameover = true;
+			Instantiate(ResetUI);
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.name == "Ball")
+		{
+			Debug.Log("OnCollisionEnter");
+			Vector3 v = other.rigidbody.velocity;
+			Vector3 ballv = Vector3.Normalize(new Vector3(v.x, 1, 1));
+			other.rigidbody.velocity = ballv * 50;
+		}
+	}
+}
